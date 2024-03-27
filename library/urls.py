282dad563f000e18bc0f1fb0aa5a4454b9f88651
SamirQT <143544211+SamirQT@users.noreply.github.com>
2024-03-27 20:07:05 +0200

    Update urls.py

--- library/urls.py
+++ library/urls.py
@@ -5,6 +5,9 @@ urlpatterns = [
     path("",views.index),
     path("book_list/",views.book_list,name="book_list"),
     path("<int:id>",views.book_detail,name="book_detail"),
+    #<int:pk>
     path("author_list/",views.author_list,name="author_list"),
-    path("<int:id>",views.author_detail,name="author_detail")
+    path("<int:id>",views.author_detail,name="author_detail
+    #<int:pk>
+
 ]
